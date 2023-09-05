fc716db343a6caa65b8f33f9942630e5eea25f3d
mobiusww <melb.ww@gmail.com>
2023-09-05 18:18:49 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit 
 import pandas
+import requests
 
 
 streamlit.title("My Parent New Healthy Diner")
@@ -26,3 +27,6 @@ streamlit.text(type(fruits_selected))
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
