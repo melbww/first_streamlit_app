43bb09b89cd3842db36e7cc07184d8ad4b6bd267
mobiusww <melb.ww@gmail.com>
2023-09-05 18:22:19 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,5 +30,10 @@ streamlit.dataframe(fruits_to_show)
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+
+
 streamlit.header("Fruityvice Fruit Advice!")
-streamlit.text(fruityvice_response.json())
+
+streamlit.dataframe(fruityvice_normalized)
+# streamlit.text(fruityvice_response.json())
