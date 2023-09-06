1a9ea861402e98d26bced61ad40003f88d437fbb
mobiusww <melb.ww@gmail.com>
2023-09-06 10:25:10 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,8 @@ import requests
 import snowflake.connector
 
 
+
+
 streamlit.title("My Parent New Healthy Diner")
 
 streamlit.header('Breakfast Menu')
@@ -41,3 +43,10 @@ streamlit.write('The user entered ', fruit_choice)
 
 streamlit.dataframe(fruityvice_normalized)
 # streamlit.text(fruityvice_response.json())
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
